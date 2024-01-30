54488f0658172a2021458062d678ae914545e7d9
thomasvilly <53155012+thomasvilly@users.noreply.github.com>
2024-01-29 22:04:44 -0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -31,6 +31,10 @@ export default async function Index() {
         </div>
       </nav>
 
+      <div>
+        <textarea>type here</textarea>
+      </div>
+
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
